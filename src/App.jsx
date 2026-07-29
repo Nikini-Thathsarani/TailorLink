8f367f57ba78e6526d6ecf450c3c1bf8ad630bdd
Nikini Thathsarani <F20020929008@student.cinec.edu>
2026-07-29 21:28:52 +0530

    feat: setup application routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import MainLayout from "./components/layout/MainLayout";
 import Home from "./pages/Home/Home";
+import AppRoutes from "./routes/AppRoutes";
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <MainLayout>
 
         <Home />
+        <AppRoutes />
 
     </MainLayout>
 
