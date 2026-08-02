151a33a026c16e2d6e2e19f6e9687f910b590ddd
Nikini Thathsarani <F20020929008@student.cinec.edu>
2026-08-02 21:53:54 +0530

    Update the Navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { NavLink } from "react-router-dom";
 import Button from "../ui/Button/Button";
 import "./Navbar.css";
@@ -5,20 +6,24 @@ import "./Navbar.css";
 const Navbar = () => {
   return (
     <header className="navbar">
+      {/* Logo */}
       <div className="logo">
         Tailor<span>Link</span>
       </div>
 
+      {/* Navigation */}
       <nav>
         <ul className="nav-links">
           <li><NavLink to="/">Home</NavLink></li>
           <li><NavLink to="/tailors">Tailors</NavLink></li>
           <li><NavLink to="/services">Services</NavLink></li>
+          <li><NavLink to="/how-it-works">How it Works</NavLink></li>
           <li><NavLink to="/about">About</NavLink></li>
         </ul>
       </nav>
 
-      <div className="nav-buttons">
+      {/* Buttons */}
+      <div className="nav-actions">
         <Button variant="outline">Login</Button>
         <Button variant="primary">Register</Button>
       </div>
